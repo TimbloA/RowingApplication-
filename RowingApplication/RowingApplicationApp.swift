6594ed3fde43a12dfd57835f12e516f26a4f0d64
Timblo A <timblo.a@etoncollege.org.uk>
2024-09-11 15:20:12 +0100

    Initial Commit

---
+++ RowingApplication/RowingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RowingApplicationApp.swift
+//  RowingApplication
+//
+//  Created by Timblo, Adi (WING) on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RowingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
