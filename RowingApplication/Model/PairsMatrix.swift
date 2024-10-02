17114a2c395db3f5d1846c7fbbdf7d222e005930
Timblo A <timblo.a@etoncollege.org.uk>
2024-10-02 15:44:30 +0100

    Pairs Matrix Start

---
+++ RowingApplication/Model/PairsMatrix.swift
@@ -0,0 +1,19 @@
+//
+//  PairsMatrix.swift
+//  RowingApplication
+//
+//  Created by Timblo, Adi (WING) on 02/10/2024.
+//
+
+import Foundation
+
+struct PairsMatrix {
+    var title: String
+    var strokeSideAthletes: [String]
+    var bowSideAthletes: [String]
+    var date = Date()
+    
+
+   
+
+}
